536f762e288b930600e558b0296959a6d4f2f754
竹田佳史 <yoshifumitakeda@takedayoshifuminoMacBook-Pro.local>
2022-09-05 01:00:06 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,51 @@
+$(function($) {
+  $('.bg-switcher').bgSwitcher({
+   images:['images/bg1.jpg','images/bg3.jpg'],
+   interval: 5000,
+   loop: true
+  });
+  $(window).scroll(function() {
+   var targetElement = $('.fadein').offset().top;
+   var scroll = $(window).scrollTop();
+   var windowHeight = $(window).height();
+   if (scroll > targetElement - windowHeight + 200){
+   $('.fadein').css('opacity','1');
+   $('.fadein').css('transform', 'tranclateY(0)');
+   }
+   });
+
+  $('.slider').slick({
+   arrows : false,
+   autoplay: true,
+   dots:true,
+   centerMode: true,
+   centerPadding: '20px',
+  });
+
+  $('.more-button').on('click', function() {
+    if ($('.open-box').is(':hidden')) {
+    $('.open-box').slideDown();
+    $(this).text('close');
+    $(this).css('background-color', '#808080');
+    }
+    else{
+    $('.open-box').slideUp();
+    $(this).text('more');
+    $(this).css('background-color', '#fabb51');
+    }
+  });
+
+  $(".modal-button").modaal();
+
+  $('nav a[href^="#"]').click(function() {
+    var adjust = 0;
+    var speed = 400;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - adjust;
+    $('body,html').animate({
+      scrollTop: position
+    }, speed, 'swing');
+    return false;
+  });
+});
